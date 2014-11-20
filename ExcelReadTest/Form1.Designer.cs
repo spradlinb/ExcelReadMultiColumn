830e3180c7539173b68c9b6081b1e72ce914c18e
spradlinb <spradlinb@gmail.com>
2014-11-19 21:28:02 -0600

    Initial sync of files

---
+++ ExcelReadTest/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ExcelReadTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(12, 12);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 0;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Enabled = false;
+            this.nextButton.Location = new System.Drawing.Point(529, 13);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 23);
+            this.nextButton.TabIndex = 1;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // filePath
+            // 
+            this.filePath.Location = new System.Drawing.Point(93, 14);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(430, 20);
+            this.filePath.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(639, 562);
+            this.Controls.Add(this.filePath);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.browseButton);
+            this.Name = "Form1";
+            this.Text = "ExcelReader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.TextBox filePath;
+
+    }
+}
+
---
+++ ExcelReadTest/Form1.cs
@@ -0,0 +1,98 @@
+﻿using Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelReadTest
+{
+    public partial class Form1 : Form
+    {
+        private Row[] _rows = null;
+        private int _rowCount = 0;
+        private TextBox[] _textBoxes = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            if (_textBoxes != null)
+            {
+                _rowCount = 0;
+                foreach (var textbox in _textBoxes)
+                    this.Controls.Remove(textbox);
+                _textBoxes = null;
+                _rows = null;
+            }
+
+            openFileDialog1.Filter = "Excel Files (*.xls,*.xlsx)|*.xls;*.xlsx";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string fileOpened = openFileDialog1.FileName;
+                try
+                {
+                    filePath.Text = fileOpened;
+                    filePath.Refresh();
+                    var worksheets = Workbook.Worksheets(fileOpened);
+                    var firstWorksheet = worksheets.FirstOrDefault();
+                    _rows = firstWorksheet.Rows;
+
+                    if (_rows != null && _rows.Length > 1)
+                    {
+                        nextButton.Enabled = true;
+                        var pt1 = 42;
+                        var colCount = _rows[0].Cells.Length;
+                        _textBoxes = new TextBox[colCount];
+                        for (var i = 0; i < colCount; i++)
+                        {
+                            TextBox newBox = new TextBox
+                            {
+                                Name = "textbox" + i.ToString(),
+                                Location = new System.Drawing.Point(13, pt1),
+                                Size = new System.Drawing.Size(400, 20),
+                                TabIndex = i + 2,
+                            };
+                            this.Controls.Add(newBox);
+                            _textBoxes[i] = newBox;
+                            pt1 += 25;
+                        }
+                        nextButton_Click(this, e);
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            var thisRow = _rows[_rowCount];
+            var colCount = thisRow.Cells.Length;
+
+            for (var i = 0; i < colCount; i++)
+            {
+                _textBoxes[i].Text = thisRow.Cells[i] != null ? thisRow.Cells[i].Text : "";
+                _textBoxes[i].Refresh();
+            }
+            _rowCount++;
+
+            if (_rows.Length <= _rowCount)
+            {
+                //this.Controls.Remove(nextButton);
+                nextButton.Enabled = false;
+            }
+        }
+    }
+}
